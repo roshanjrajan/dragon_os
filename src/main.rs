450a601ea06d17d67dcd235d4cb1922ffefe7727
Roshan Rajan <roshanjrajan@gmail.com>
2022-02-27 19:37:21 -0600

    Finishing blog_os post 4

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
-
-mod vga_buffer;
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(dragon_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use dragon_os::println;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
+
+    panic!();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    panic!("Some panic message");
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    dragon_os::test_panic_handler(info)
 }
